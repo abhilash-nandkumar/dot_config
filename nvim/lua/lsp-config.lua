63b03ab248c49c5658972e914341bf3955a50955
Abhilash Nand Kumar <abhilash.nandkumar@node-robotics.com>
2022-03-29 13:07:09 +0200

    added lua lsp

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -42,6 +42,36 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+local sumneko_root_path = os.getenv("HOME") ..
+                              "/.config/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+nvim_lsp.sumneko_lua.setup({
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
+            completion = {enable = true, callSnippet = "Both"},
+            diagnostics = {
+                enable = true,
+                globals = {'vim', 'describe'},
+                disable = {"lowercase-global"}
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                    [vim.fn.expand('/usr/share/awesome/lib')] = true
+                },
+                -- adjust these two values if your performance is not optimal
+                maxPreload = 2000,
+                preloadFileSize = 1000
+            }
+        }
+    },
+    on_attach = on_attach_common
+})
+
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
