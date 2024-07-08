8db137e9dfe8f4e14aaddfa9e16184f6718ef37d
Abhilash <abhilash.nandkumar@gmail.com>
2024-07-08 16:49:33 +0200

    chore: update auto completion scrolling

--- nvim/lua/autocompletion.lua
+++ nvim/lua/autocompletion.lua
@@ -24,8 +24,8 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		["<C-j>"] = cmp.mapping.select_prev_item(),
-		["<C-k>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
