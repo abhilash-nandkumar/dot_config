9068e54a29fb9126978b80d6f479e4c9670e1c05
Abhilash Nand Kumar <abhilash.nandkumar@gmail.com>
2025-03-01 13:31:52 +0100

    chore: fix completion configs

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,19 +1,15 @@
--- lspkind
--- local ok, lspkind = pcall(require, "lspkind")
--- if not ok then
---   return
--- end
-
--- lspkind.init()
-
--- nvim-cmp setup
 local cmp = require("cmp")
-options = {
+local luasnip = require("luasnip")
+
+local options = {
+	completion = { completeopt = "menu,menuone" },
+
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
+
 	mapping = {
 		["<C-k>"] = cmp.mapping.select_prev_item(),
 		["<C-j>"] = cmp.mapping.select_next_item(),
@@ -25,44 +21,36 @@ options = {
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
-		["<Tab>"] = function(fallback)
+		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
-		end,
-		["<S-Tab>"] = function(fallback)
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
-		end,
+		end, { "i", "s" }),
 	},
-	-- nvim-cmp by defaults disables autocomplete for prompt buffers
-	enabled = function()
-		return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
-	end,
+
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
-		-- { name = "nvim_lua" },
+		{ name = "nvim_lua" },
 		-- { name = "path" },
 		{ name = "buffer", keyword_length = 5 },
+		{ name = "cmdline" },
 	},
-	-- formatting = {
-	-- 	format = lspkind.cmp_format({
-	-- 		with_text = true,
-	-- 		menu = {
-	-- 			buffer = "[buf]",
-	-- 			path = "[path]",
-	-- 			luasnip = "[snip]",
-	-- 		},
 }
 
-return optionsy
+-- return vim.tbl_deep_extend("force", options, require("cmp").opts)
+
+return options
