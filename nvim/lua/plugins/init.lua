0a1117b07d7f9360aff50b6226ad255d03957b38
Abhilash Nand Kumar <abhilash.nandkumar@gmail.com>
2025-03-07 20:49:37 +0100

    chore: add auto pairing for brackets

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -109,6 +109,11 @@ return {
 	},
 
 	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
