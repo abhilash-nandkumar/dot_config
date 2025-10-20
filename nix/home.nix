3f4aa5510d7ca1c03af261a455e97ad1643e3e04
Abhilash <abhilash.nandkumar@gmail.com>
2025-10-20 23:58:16 +0100

    chore: add nix configs

---
+++ nix/home.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "abhilash";
+  home.homeDirectory = "/home/abhilash";
+
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+
+    # GUI apps
+    chromium
+    gimp
+
+    # dev tools
+    neovim
+    yazi
+    kitty
+    tmux
+    zsh
+
+    # CLI 
+    docker
+    docker-compose
+    git-lfs
+    openvpn
+    pre-commit
+    htop
+    btop
+
+    # other
+    fzf
+    ripgrep
+    cargo
+
+    # Fonts & icons
+    starship
+  ];
+
+  home.file = {
+    ".tmux.conf" = {
+    	source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/dot_config/tmux/.tmux.conf";
+     };
+
+    ".zshrc" = {
+      	source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/dot_config/zsh/.zshrc";
+     };
+
+    ".config/nvim" = {
+    	source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/git/dot_config/nvim";
+     };
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      settings.user.name = "Abhilash";
+      settings.user.email = "abhilash.nandkumar@gmail.com";
+	  lfs.skipSmudge = true;
+    };
+  };
+}
